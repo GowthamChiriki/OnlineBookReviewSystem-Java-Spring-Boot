cc997a65a7603097d2558a624ee8c72e4a15eddd
GOWTHAM SAI CHIRIKI <gowthamsai167@gmail.com>
2025-10-08 02:18:26 +0530

    I have written code for the classes Book and Review and committed here.

--- src/main/java/com/example/bookreviewsystem/entity/Book.java
+++ src/main/java/com/example/bookreviewsystem/entity/Book.java
@@ -1,4 +1,26 @@
 package com.example.bookreviewsystem.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String genre;
+    private Integer publicationYear;
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews;
+
 }
--- src/main/java/com/example/bookreviewsystem/entity/Review.java
+++ src/main/java/com/example/bookreviewsystem/entity/Review.java
@@ -1,4 +1,35 @@
 package com.example.bookreviewsystem.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Reviewer Name is required")
+    private String reviewerName;
+
+    @Min(1)
+    @Max(5)
+    private int rating;
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+
 }
