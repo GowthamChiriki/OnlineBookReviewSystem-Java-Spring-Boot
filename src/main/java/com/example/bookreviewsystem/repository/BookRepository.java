e770907f59effa6399cf1cda809bb3759af17fcf
GOWTHAM SAI CHIRIKI <gowthamsai167@gmail.com>
2025-10-08 02:25:28 +0530

    Implemented the code for the classes in repository layer.

--- src/main/java/com/example/bookreviewsystem/repository/BookRepository.java
+++ src/main/java/com/example/bookreviewsystem/repository/BookRepository.java
@@ -1,4 +1,15 @@
 package com.example.bookreviewsystem.repository;
+import com.example.bookreviewsystem.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class BookRepository {
+import java.util.List;
+
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByTitleContainingIgnoreCase(String title);
+    List<Book> findByAuthorContainingIgnoreCase(String author);
+    List<Book> findByGenreContainingIgnoreCase(String genre);
+
 }
--- src/main/java/com/example/bookreviewsystem/repository/ReviewRepository.java
+++ src/main/java/com/example/bookreviewsystem/repository/ReviewRepository.java
@@ -1,4 +1,13 @@
 package com.example.bookreviewsystem.repository;
+import com.example.bookreviewsystem.entity.Review;
 
-public class ReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByReviewerNameContainingIgnoreCase(String reviewerName);
+    List<Review> findByCommentContainingIgnoreCase(String comment);
+    List<Review> findByBookId(Long bookId);
 }
